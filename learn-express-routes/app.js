c3046facc7adfca2bd9d7ce5782a4238456b6b8a
Kieran Dunch <dunch.kieran@gmail.com>
2024-03-18 08:39:51 -0600

    added get request

--- learn-express-routes/app.js
+++ learn-express-routes/app.js
@@ -6,3 +6,23 @@ const app = express();
 const PORT = process.env.PORT || 4001;
 
 // Invoke the app's `.listen()` method below:
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+
+// Get request
+
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
+// Open a call to `app.get()` below:
+app.get('/expressions', (req, res, next) => {
+  console.log(req);
+  // res.send(expressions);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
